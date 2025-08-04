b64a54da39b679b2a5ada56119d0c5e421142ce5
Vbif322 <hxdfue@yandex.ru>
2025-08-04 23:23:13 +0300

    add auth provider

---
+++ src/app/actions/auth.ts
@@ -0,0 +1,21 @@
+import { signupFormSchema, FormState } from "@/app/lib/definitions";
+import { signIn } from "@/auth";
+
+export async function signup(state: FormState, formData: FormData) {
+  // Validate form fields
+  const validatedFields = signupFormSchema.safeParse({
+    name: formData.get("name"),
+    email: formData.get("email"),
+    password: formData.get("password"),
+  });
+
+  // If any form fields are invalid, return early
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+    };
+  }
+  signIn("credentials", formData);
+
+  // Call the provider or db to create a user...
+}
--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,36 @@
 import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { signupFormSchema } from "./app/lib/definitions";
+import { ZodError } from "zod";
+import bcrypt from "bcrypt";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        try {
+          let user;
+          const { email, password } = await signupFormSchema.parseAsync(
+            credentials
+          );
+          const pwHash = await bcrypt.hash(password, 10);
+          user = await getUserFromDb(email, pwHash);
+
+          if (!user) {
+            throw new Error("Invalid credentials.");
+          }
+          return user;
+        } catch (error) {
+          if (error instanceof ZodError) {
+            // Return `null` to indicate that the credentials are invalid
+            return null;
+          }
+        }
+      },
+    }),
+  ],
 });
