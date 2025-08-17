23f9af2a1192b5043013e6b2fc7cdc59b6299a7d
Vbif322 <hxdfue@yandex.ru>
2025-08-18 00:07:33 +0300

    add nav block + main block

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,35 @@
-import styles from "./page.module.css";
+import s from "./page.module.css";
+import Image from "next/image";
+import Kirill from "../../public/Kirill.webp";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>Привет</main>
+    <div className={s.page}>
+      <main className={s.main}>
+        <nav className={s.nav}>
+          <ul className={s.navigation}>
+            <p className={s.logoTitle}>Бизнес с Кириллом Месеняшиным</p>
+            <li>О преподавателе</li>
+            <li>Курсы</li>
+            <li>Отзывы</li>
+            <button className={s.button}>Вход</button>
+          </ul>
+        </nav>
+        <section className={s.mainSection}>
+          <div className={s.mainBlock}>
+            <h1 className={s.title}>
+              Онлайн-курсы по бизнесу от Кирилла Месеняшина
+            </h1>
+            <span className={s.subtitle}>
+              Пошаговые программы для запуска и развития бизнеса
+            </span>
+            <button className={s.button}>Выбрать курс</button>
+          </div>
+          <div className={s.imgContainer}>
+            <Image src={Kirill} alt="Главное фото Кирилла" width={500} />
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
