ed74c175aaf5ac1c184e8ffd97d818407c60f3b4
Vbif322 <hxdfue@yandex.ru>
2025-08-23 21:58:18 +0300

    add new sections to landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,28 @@ import Image from "next/image";
 import Kirill from "../../public/Kirill.webp";
 import { getUser } from "./lib/dal";
 import { redirect } from "next/navigation";
+import CourseCard from "./ui/course-card/CourseCard";
+
+const courses = [
+  {
+    id: 1,
+    title: "Создание бизнеса с нуля",
+    description: "Полный список курсов по запуску и развитию бизнеса",
+    href: "/course",
+  },
+  {
+    id: 2,
+    title: "Создание бизнеса с нуля",
+    description: "Полный список курсов по запуску и развитию бизнеса",
+    href: "/course",
+  },
+  {
+    id: 3,
+    title: "Создание бизнеса с нуля",
+    description: "Полный список курсов по запуску и развитию бизнеса",
+    href: "/course",
+  },
+];
 
 export default function Home() {
   // const user = await getUser();
@@ -21,7 +43,7 @@ export default function Home() {
             <li>О преподавателе</li>
             <li>Курсы</li>
             <li>Отзывы</li>
-            <button className={s.button} onClick={onClick}>
+            <button onClick={onClick}>
               {false ? "Личный кабинет" : "Вход"}
             </button>
           </ul>
@@ -34,10 +56,37 @@ export default function Home() {
             <span className={s.subtitle}>
               Пошаговые программы для запуска и развития бизнеса
             </span>
-            <button className={s.button}>Выбрать курс</button>
+            <button style={{ maxWidth: "300px" }}>Выбрать курс</button>
           </div>
           <div className={s.imgContainer}>
-            <Image src={Kirill} alt="Главное фото Кирилла" width={500} />
+            <Image src={Kirill} alt="Главное фото преподавателя" width={350} />
+          </div>
+        </section>
+        <section className={s.section}>
+          <h3 className={s.sectionTitle}>О преподавателе</h3>
+          <div className={s.mainInfoContainer}>
+            <Image
+              src={Kirill}
+              alt="Маленькое фото преподавателя"
+              width={200}
+            />
+            <p className={s.mainInfoText}>
+              <b>Кирилл Месеняшин</b> - эксперт-практик с 20-летним опытом в
+              области организационного развития и совершенствования cистем
+              управления
+            </p>
+          </div>
+        </section>
+        <section className={s.section}>
+          <h3 className={s.sectionTitle}>Каталог курсов</h3>
+          <div className={s.courseCardContainer}>
+            {courses.map((course) => (
+              <CourseCard
+                key={course.id}
+                {...course}
+                href={course.href + "/" + course.id}
+              />
+            ))}
           </div>
         </section>
       </main>
